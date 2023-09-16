608db822067d17f8c2dbcdcfa1f9427981003769
Javier Aguilar <josuejaviera19@gmail.com>
2023-09-15 23:09:43 -0600

    Registrar Usuario

--- Frontend/src/app/create-user/create-user.component.ts
+++ Frontend/src/app/create-user/create-user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-user.component.css']
 })
 export class CreateUserComponent {
+  formData = {
+    username: '',
+    nombres: '',
+    apellidos: '',
+    correo: '',
+    password: '', 
+    passwordConfirm: ''
+  };
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    // Aquí puedes agregar la lógica para enviar los datos del formulario al servidor
+    console.log('Datos del formulario enviados:', this.formData);
+    this.formData = {
+      username: '',
+      nombres: '',
+      apellidos: '',
+      correo: '',
+      password: '', 
+      passwordConfirm: ''
+    };
+    // Puedes hacer una solicitud HTTP aquí para autenticar al usuario
+    // y redirigirlo a la página de inicio de sesión exitosa.
+  }
+  
 }
