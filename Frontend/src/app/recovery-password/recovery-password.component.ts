ac3319f02f1f68bf6cd35112eb9d61a9ecb01d71
Javier Aguilar <josuejaviera19@gmail.com>
2023-09-15 20:19:13 -0600

    Cambiar Contarseña

--- Frontend/src/app/recovery-password/recovery-password.component.ts
+++ Frontend/src/app/recovery-password/recovery-password.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-recovery-password',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./recovery-password.component.css']
 })
 export class RecoveryPasswordComponent {
+  formData = {
+    username: '',
+    password: '', 
+    passwordConfirm: '',
+    correo: ''
+  };
 
+  constructor(private formBuilder: FormBuilder) {}
+
+  onSubmit() {
+    // Aquí puedes agregar la lógica para enviar los datos del formulario al servidor
+    console.log('Datos del formulario enviados:', this.formData);
+    this.formData = {
+      username: '',
+      password: '', 
+      passwordConfirm: '',
+      correo: ''
+    };
+    // Puedes hacer una solicitud HTTP aquí para autenticar al usuario
+    // y redirigirlo a la página de inicio de sesión exitosa.
+  }
 }
