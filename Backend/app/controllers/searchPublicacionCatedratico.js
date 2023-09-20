08e274fddeab0647f8b7428933676c0a9d78e719
SamuelAguilar18 <88750058+SamuelAguilar18@users.noreply.github.com>
2023-09-20 15:39:15 -0600

    Buscar publicación

---
+++ Backend/app/controllers/searchPublicacionCatedratico.js
@@ -0,0 +1,37 @@
+const mysql = require('mysql2/promise');
+const conn = require('../config/connection');
+
+async function searchPublicacionCatedratico(req, res) {
+  try {
+    const connection = await mysql.createConnection(conn.config.connection);
+    const [rows] = await connection.query(`
+      SELECT
+        p.id,
+        p.fecha,
+        p.mensaje,
+        p.usuario,
+        c.nombre AS curso,
+        ct.nombre AS catedratico
+      FROM publicacion AS p
+      INNER JOIN curso AS c ON p.curso = c.id
+      INNER JOIN catedratico AS ct ON p.catedratico = ct.id
+      WHERE LOWER(ct.nombre) LIKE ?
+      ORDER BY p.fecha, p.id DESC
+    `, [`%${req.body.nombre.toLowerCase()}%`]); 
+    connection.end();
+    
+    const publicaciones = rows; 
+    
+    if (!publicaciones || publicaciones.length === 0) {
+      return res.json([]);
+    }
+    
+    res.json(publicaciones);
+  } catch (error) {
+    console.error('Error al consultar la base de datos:', error);
+    res.status(500).json({ error: 'Error en el servidor', publicaciones: [] }); 
+  }
+}
+
+module.exports = { searchPublicacionCatedratico };
+
--- Frontend/src/app/search-post/search-post.component.ts
+++ Frontend/src/app/search-post/search-post.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { LogicaService } from 'src/app/logica/logica.service';
 import { Router } from '@angular/router';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-search-post',
@@ -8,7 +9,33 @@ import { Router } from '@angular/router';
   styleUrls: ['./search-post.component.css']
 })
 export class SearchPostComponent {
-  constructor(private router: Router, private analizarService: LogicaService) {}
+  selectedCategory: string = 'Curso';
+  postName: string = '';
+  results: any[] = []; 
+
+  constructor(private router: Router, private analizarService: LogicaService, private datePipe: DatePipe) {}
+
+  buscarPost() {
+    const data = {
+      nombre: this.postName
+    }
+
+    if(this.selectedCategory === 'Curso'){
+      this.analizarService.searchPublicacionCurso(data).subscribe(data => {
+        this.results = data;
+      })
+
+    }else{
+      this.analizarService.searchPublicacionCatedratico(data).subscribe(data => {
+        this.results = data;
+      })
+    }
+  }
+
+  verPost(id: any) {
+    this.analizarService.guardarPost(id);
+    this.router.navigate(['/post'])
+  }
 
   getUserName(): string {
     return this.analizarService.getUsername()
@@ -19,3 +46,5 @@ export class SearchPostComponent {
     this.router.navigate(['/login'])
   }
 }
+
+
